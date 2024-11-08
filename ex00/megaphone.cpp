e0ffde95b110e644787b478d9725e162554cb77c
Berke Ibrahim Ince <bince@made-f0Ar4s12.clusters.42paris.fr>
2024-11-08 14:46:42 +0100

    all exercises are done

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,6 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bince < bince@student.42.fr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/08 14:45:40 by bince             #+#    #+#             */
+/*   Updated: 2024/11/08 14:45:41 by bince            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
 int main(int argc, char **argv)
 {
     if (argc < 2)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
         return 1;
-    
+	}
+    for (int i = 1; i < argc; i++)
+	{
+		for(int j = 0; argv[i][j]; j++)
+		{
+			argv[i][j] = std::toupper(argv[i][j]);
+		}
+		std::cout << argv[i];
+	}
+	std::cout << std::endl;
 }
--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bince < bince@student.42.fr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/08 14:46:16 by bince             #+#    #+#             */
+/*   Updated: 2024/11/08 14:46:16 by bince            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Account.hpp"
 #include <ctime>
 #include <iostream>
@@ -17,7 +29,14 @@ Account::Account(int init_deposit) : _accountIndex(_nbAccounts), _amount(init_de
 	this->_totalAmount += this->_amount;
 }
 
-Account::~Account(){}
+Account::~Account()
+{
+	Account::_nbAccounts--;
+	Account::_totalAmount -= this->_amount;
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";closed" << std::endl;
+	return ;
+}
 
 void Account::displayAccountsInfos()
 {
@@ -36,8 +55,57 @@ void Account::displayStatus() const
 	std::cout << "deposits:" << this->_nbDeposits << ";";
 	std::cout << "withdrawals:" << this->_nbWithdrawals << ";" << std::endl;
 }
+void Account::makeDeposit(int deposit)
+{
+	this->_amount += deposit;
+	this->_nbDeposits += 1;
+	Account::_totalAmount += deposit;
+	Account::_totalNbDeposits += 1;
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount - deposit << ";deposit:"
+		<< deposit << ";amount:" << this->_amount << ";nb_deposits:" << this->_nbDeposits << std::endl;
+}
 
+bool Account::makeWithdrawal(int withdrawal)
+{
+	Account::_displayTimestamp();
+	if (withdrawal > this->_amount)
+	{
+		std::cout << "index:" << this->_accountIndex  << ";p_amount:" << this->_amount << ";withdrawal:refused" << std::endl;
+		return false;
+	}
+	this->_amount -= withdrawal;
+	this->_nbWithdrawals += 1;
+	Account::_totalAmount -= withdrawal;
+	Account::_totalNbWithdrawals += 1;
+	std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount + withdrawal << ";withdrawal:"
+		<< withdrawal << ";amount:" << this->_amount << ";nb_withdrawals:" << this->_nbWithdrawals << std::endl;
+	return true;
+}
 
+int	Account::checkAmount( void ) const
+{
+	return this->_amount;
+}
+
+int Account::getNbAccounts()
+{
+	return (Account::_nbAccounts);
+}
+int Account::getNbDeposits()
+{
+	return (Account::_totalNbDeposits);
+}
+
+int Account::getTotalAmount()
+{
+	return (Account::_totalAmount);
+}
+
+int Account::getNbWithdrawals()
+{
+	return (Account::_totalNbWithdrawals);
+}
 
 void Account::_displayTimestamp()
 {
